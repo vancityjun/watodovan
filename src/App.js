d5e114e9c1a7b923b172c35be3cc2cb9f253b98a
vancityjun <vancityjun@github.com>
2019-09-26 19:39:40 -0700

    added authors' name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import EventPage from "./pages/EventPage/EventPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    eventData: [],
+    categoryData: []
+  };
+  componentDidMount() {
+    axios
+      .all([
+        axios.get(
+          "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.address=Vancouver%2C+Canada&location.within=40km&token=KCQPMHOXUOJBGVFFKKUV"
+        ),
+        axios.get(
+          "https://www.eventbriteapi.com/v3/categories/?token=KCQPMHOXUOJBGVFFKKUV"
+        )
+      ])
+      .then(
+        axios.spread((data, categoryData) => {
+          const { events } = data;
+          if (events && events.length > 0) {
+            this.setState({ eventData: events });
+          }
+          const { categories } = categoryData;
+          if (categories && categories.length > 0) {
+            this.setState({ categoryData: categories });
+          }
+          console.log("categoryData:", categoryData);
+          console.log("data:", data);
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <EventPage />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/EventPage/EventPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Layout, Row } from "antd";
+
+class EventPage extends Component {
+  render() {
+    return (
+      <>
+        <Layout className="">
+          {/* NavBar */}
+          <Layout.Content>
+            {/* featured */}
+            {/* <Row>{eventCards}</Row> */}
+          </Layout.Content>
+          <Layout.Footer>
+            WaToDo Vancouver ©2019 Created by WTD Van
+          </Layout.Footer>
+        </Layout>
+      </>
+    );
+  }
+}
+
+export default EventPage;
